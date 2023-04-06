404ed6c8e557f998adc21414bcf0c44a7f1a078d
Santhosh Reddy Vantikommu <s560607@nwmissouri.edu>
2023-04-05 21:50:02 -0500

    added content

--- routes/rose.js
+++ routes/rose.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('rose', { title: 'Search Results Kite' });
+  res.render('rose', { title: 'Search Results Rose' });
 });
 
 module.exports = router;
